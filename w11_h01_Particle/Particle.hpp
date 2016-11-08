3156f804e10a50b906ea06ba882d8d9baab36523
Ziqiang1226 <1219044741@qq.com>
2016-11-08 18:13:20 -0500

    Create Particle.hpp

---
+++ w11_h01_Particle/Particle.hpp
@@ -0,0 +1,18 @@
+
+
+#pragma once
+#include "ofMain.h"
+
+class Particle{
+public:
+
+    Particle(ofVec2f pos);
+    
+    void resetForces();
+    void applyForce(ofVec2f force);
+    void update(float multiplier);
+    void draw();
+    
+    ofVec2f mPosition, mVelocity, mAcceleration;
+    float mLifeSpan;
+};
